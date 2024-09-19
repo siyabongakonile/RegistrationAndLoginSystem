a7ec809ee9e5c18cdb72c78bb4f46638eb3866a6
Siyabonga Konile <91778630+siyabongakonile@users.noreply.github.com>
2024-09-19 04:49:05 +0200

    Update profile.php

--- views/profile.php
+++ views/profile.php
@@ -6,6 +6,21 @@
     <title>User Profile</title>
 </head>
 <body>
+    <div class="user-cont">
+        <?php if(isset($message)): ?>
+        <div class="message-cont">
+            <p><?= $message ?></p>
+        </div>
+        <?php endif ?>
+        <h2>Welcome, <?= $user->getFirstname() ?></h2>
+        <p>Firstname: <?= $user->getFirstname() ?></p>
+        <p>Lastname: <?= $user->getLastname() ?></p>
+        <p>Email: <?= $user->getEmail() ?></p>
+        <p>
+            Is Email Verified: <?php echo ($user->getIsEmailVerified()? 'True': 'False') ?>
+            <a href="/sendverification?user-id=<?= $user->getId() ?>">Verify Email</button>
+        </p>
+    </div>
     <a href="/logout">Logout</a>
 </body>
 </html>
