545d254a66a86684e87b9dfc2f844fbb44318f88
Siyabonga Konile <91778630+siyabongakonile@users.noreply.github.com>
2024-09-21 06:49:24 +0200

    Update Email.php

--- App/Email.php
+++ App/Email.php
@@ -3,14 +3,21 @@ declare(strict_types = 1);
 
 namespace App;
 
+use \App\Model\User;
+
 class Email{
-    public static function sendEmailVerification(string $token): bool{
-        $to = '';
-        $subject = '';
+    public static function sendEmailVerification(string $token, User $user, string $link): bool{
+        $to = $user->getEmail();
+        $subject = 'RegLog: Email Verification';
+        $helloMsg = "Hello {$user->getFirstname()}";
+        $completeLink = "{$link}?email={$user->getEmail()}&token={$token}";
         $message = <<<_TEXT
+$helloMsg,
+Your email address verification link is $completeLink.
 
+Thank you.
 _TEXT;
-
+        \Logging\logInfo("Sending Email");
         return mail($to, $subject, $message);
     }
 }
