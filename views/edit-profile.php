8f9648847a747a2485d797e90ff3068acf32086f
Siyabonga Konile <91778630+siyabongakonile@users.noreply.github.com>
2024-09-21 06:49:18 +0200

    Update edit-profile.php

---
+++ views/edit-profile.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Profile</title>
+</head>
+<body>
+    <div class="edit-cont">
+        <div class="edit-inner">
+            <?php if(isset($errors) && count($errors) > 0): ?>
+            <div class="errors-cont">
+                <ul>
+                    <?php foreach($errors as $error): ?>
+                        <li><?= $error ?></li>
+                    <?php endforeach ?>
+                </ul>
+            </div>
+            <?php endif ?>
+
+            <div class="form-cont">
+                <form action="/user/edit" method="post">
+                    <div class="form-row">
+                        <label for="fname">
+                            Firstname: <input type="text" name="fname" id="fname">
+                        </label>
+                    </div>
+                    <div class="form-row">
+                        <label for="lname">
+                            Lastname: <input type="text" name="lname" id="lname">
+                        </label>
+                    </div>
+                    <div class="form-row">
+                        <label for="email">
+                            Email: <input type="text" name="email" id="email">
+                        </label>
+                    </div>
+                    <div class="form-row">
+                        <label for="password">
+                            Password: <input type="text" name="password" id="password">
+                        </label>
+                    </div>
+                    <div class="form-row">
+                        <label for="confirm-password">
+                            Confirm Password: <input type="text" name="confirm-password" id="confirm-password">
+                        </label>
+                    </div>
+                    <div class="form-row">
+                        <input type="submit" value="Update">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
