255d0721a87cc1d8700994236f6cbd578a71f108
Siyabonga Konile <91778630+siyabongakonile@users.noreply.github.com>
2024-09-22 09:25:06 +0200

    Update Config.example.php

--- inc/Config.example.php
+++ inc/Config.example.php
@@ -1,8 +1,8 @@
 <?php
 # Database configuration.
-define('DB_PLATFORM', '');
-define('DB_HOST', '');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
-define('DB_NAME', '');
-define('DB_CHARSET', 'utf8mb4');
+// define('DB_PLATFORM', '');
+// define('DB_HOST', '');
+// define('DB_USERNAME', '');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', '');
+// define('DB_CHARSET', 'utf8mb4');
