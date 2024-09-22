245c623f8f415b1a761eeb5cec6833625f43a930
Siyabonga Konile <91778630+siyabongakonile@users.noreply.github.com>
2024-09-23 00:32:55 +0200

    Update Email.php

--- App/Email.php
+++ App/Email.php
@@ -6,18 +6,66 @@ namespace App;
 use \App\Model\User;
 
 class Email{
-    public static function sendEmailVerification(string $token, User $user, string $link): bool{
-        $to = $user->getEmail();
-        $subject = 'RegLog: Email Verification';
-        $helloMsg = "Hello {$user->getFirstname()}";
-        $completeLink = "{$link}?email={$user->getEmail()}&token={$token}";
+    static string $headers = "From: noreply@reglog.user\r\n";
+
+    public static function sendEmailVerification(User $user, string $token, string $link): bool{
+        $sendEmailTo = $user->getEmail();
+        $emailSubject = APP_NAME . ': Email Verification';
+        $firstname = $user->getFirstname();
+        $completeLink = static::createCompleteEmailVerificationLink($link, $sendEmailTo, $token);
         $message = <<<_TEXT
-$helloMsg,
+Hello $firstname,
 Your email address verification link is $completeLink.
 
 Thank you.
 _TEXT;
         \Logging\logInfo("Sending Email");
-        return mail($to, $subject, $message);
+        return mail($sendEmailTo, $emailSubject, $message, static::$headers);
+    }
+
+    public static function sendRegistrationEmail(User $user): bool{
+        $sendEmailTo = $user->getEmail();
+        $emailSubject = APP_NAME . ": Registration";
+        $firstname = $user->getFirstname();
+        $message = <<<_TEXT
+Hi $firstname, 
+welcome to RegLog.user! 
+
+Thank you.
+_TEXT;
+        \Logging\logInfo("Sending Registration Email");
+        return mail($sendEmailTo, $emailSubject, $message, static::$headers);
+    }
+
+    public static function sendUserDeletionEmail(User $user): bool{
+        $sendEmailTo = $user->getEmail();
+        $emailSubject = APP_NAME . ": Account Deletion";
+        $firstname = $user->getFirstname();
+        $message = <<<_TEXT
+Good day $firstname,
+sad to see you leave. Your account has been deleted successfully.
+
+Thank you.
+_TEXT;
+        \Logging\logInfo("Sending Deletion");
+        return mail($sendEmailTo, $emailSubject, $message, static::$headers);
+    }
+
+    public static function sendEmailUpdateEmail(User $user, string $emailVerificationToken, string $verificationEndpoint): bool{
+        $sendEmailTo = $user->getEmail();
+        $emailSubject = APP_NAME . ": Email Update";
+        $firstname = $user->getFirstname();
+        $completeLink = static::createCompleteEmailVerificationLink($verificationEndpoint, $sendEmailTo, $emailVerificationToken);
+        $message = <<<_TEXT
+Hello $firstname,
+an update on you email address on our platform was made 
+and you can verify your new address using this link: $completeLink.
+_TEXT;
+        \Logging\logInfo("Sending email update email");
+        return mail($sendEmailTo, $emailSubject, $message, static::$headers);
+    }
+
+    protected static function createCompleteEmailVerificationLink(string $verificationEndpoint, string $userEmail, string $verificationToken): string{
+        return "{$verificationEndpoint}?email={$userEmail}&token={$verificationToken}";
     }
 }
