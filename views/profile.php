506acf8bc25c839dff63462bb675069284cc7c2a
Siyabonga Konile <91778630+siyabongakonile@users.noreply.github.com>
2024-09-24 00:12:39 +0200

    Update profile.php

--- views/profile.php
+++ views/profile.php
@@ -7,20 +7,18 @@
 </head>
 <body>
     <div class="user-cont">
-        <?php if(isset($message)): ?>
-        <div class="message-cont">
-            <p><?= $message ?></p>
-        </div>
-        <?php endif ?>
+        <?php include_once __DIR__ . "/patials/message.php" ?>
         <h2>Welcome, <?= $user->getFirstname() ?></h2>
         <p>Firstname: <?= $user->getFirstname() ?></p>
         <p>Lastname: <?= $user->getLastname() ?></p>
         <p>Email: <?= $user->getEmail() ?></p>
         <p>
             Is Email Verified: <?php echo ($user->getIsEmailVerified()? 'True': 'False') ?>
+            <?php if(!$user->getIsEmailVerified()): ?>
             <a href="/sendverification?user-id=<?= $user->getId() ?>">Verify Email</button>
+            <?php endif ?>
         </p>
     </div>
-    <a href="/logout">Logout</a>
+    <a href="/user/edit">Edit Profile</a> | <a href="/user/delete">Delete Account</a> | <a href="/logout">Logout</a>
 </body>
 </html>
