9352eebc52c2b924d4b5f952e8400c6663faa7f6
Siyabonga Konile <91778630+siyabongakonile@users.noreply.github.com>
2024-09-23 20:26:11 +0200

    Update Config.php

--- inc/Config.php
+++ inc/Config.php
@@ -6,3 +6,8 @@ define('DB_USERNAME', 'reglog');
 define('DB_PASSWORD', 'reglog');
 define('DB_NAME', 'reglog');
 define('DB_CHARSET', 'utf8mb4');
+
+# App configuration
+define('APP_NAME', 'RegLog');
+define('APP_DOMAIN', 'RegLog.user');
+define('EMAIL_VERIFICATION_DOMAIN', 'http://reglog.user/verify');
